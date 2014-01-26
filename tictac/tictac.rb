2d59b80a4f6324a9ade72199c68a42fba2fe6929
Christine Yu <cyu@mstar.io>
2014-01-25 19:36:18 -0800

    fix make_move and create check_board method

--- tictac/tictac.rb
+++ tictac/tictac.rb
@@ -28,19 +28,82 @@ class TicTacGame
   end
 
 	def make_move(player, move_pos)
-		@board = @board.map.with_index do |row, row_index|
-			row = row.map.with_index do |position, column_index|
-				position_num = get_position_num(row_index, column_index)
+		for i in 0..@board.size-1
+			for j in 0..@board[i].size-1
+				position_num = get_position_num(i, j)
 				if position_num == move_pos
-					if position.nil?
-						puts "setting position #{position_num} to player"
-						position = player
+					if @board[i][j].nil?
+						@board[i][j] = player	
+						return
 					else
 						puts "position already filled"
 					end
 				end
 			end
 		end
+		puts "illegal position"
+ #   @board = @board.map.with_index do |row, row_index|
+			#row = row.map.with_index do |position, column_index|
+				#position_num = get_position_num(row_index, column_index)
+				#if position_num == move_pos
+					#if position.nil?
+						#puts "setting position #{position_num} to player"
+						#position = player
+					#else
+						#puts "position already filled"
+					#end
+				#end
+			#end
+	#  end
+	end
+
+	def check_board()
+		#check wins in rows
+		for i in 0..@board.size-1
+			holder = @board[i][0]
+			next if holder.nil?
+			mismatched = false
+			for j in 1..@board[i].size-1
+				if (not holder.nil?) && holder != @board[i][j]
+					mismatched = true
+					break
+				end
+			end
+			if not mismatched
+				end_game(holder)
+				return
+			end
+		end
+
+		#check wins in columns
+		for i in 0..@board[0].size-1
+			holder = @board[0][i]
+			next if holder.nil?
+			mismatched = false
+			for j in 1..@board.size-1
+				if (not holder.nil?) && holder != @board[j][i]
+					mismatched = true
+					break
+				end
+			end
+			if not mismatched
+				end_game(holder)
+				return
+			end
+		end
+
+		#check diagonal wins
+		if not @board[1][1].nil?
+			if @board[0][0] == @board[1][1] && @board[1][1] == @board[2][2]
+				end_game(@board[0][0])
+				return
+			end
+			if @board[1][2] == @board[1][1] && @board[1][1] == @board[0][2]
+				end_game(@board[1][2])
+				return
+			end
+		end
+
 	end
 
   attr_accessor :playerX, :playerO, :ended
@@ -48,6 +111,11 @@ class TicTacGame
 	private
 	def get_position_num(n, j)
 		return (n*@board.size)+j+1
+	end
+
+	def end_game(winner)
+		puts "#{winner.name} as #{winner.symbol} won!"
+		@ended = true
 	end
 
 end
@@ -84,6 +152,6 @@ while(!game.game_ended?)
 	pos = gets.chomp.to_i
 	game.make_move(pO, pos)
 	game.draw_board
-	game.ended = true
+	game.check_board
 end
 
